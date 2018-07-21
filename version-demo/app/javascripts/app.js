13749261afc4e9c08529f510504dead6bf39889f
Tuan Anh <tuananh11594@gmail.com>
2018-07-22 02:21:40 +0700

    changed name folder chapter1 -> version-vote-eth chapter2->version-vote-token chapter3->version-demo

--- version-demo/app/javascripts/app.js
+++ version-demo/app/javascripts/app.js
@@ -107,6 +107,27 @@ window.App = {
     });
   },
 
+  validateNumber: function(evt) {
+    var theEvent = evt || window.event;
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode( key );
+    var regex = /[0-9]|\ /;
+    if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+  },
+  
+  validateName: function(evt) {
+    var theEvent = evt || window.event;
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode( key );
+    var regex = /[0-9]/;
+    if( regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+  },
 };
 
 window.addEventListener('load', function () {
